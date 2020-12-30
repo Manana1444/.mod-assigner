8e95e13c12c3f81189ff4e29f0fd92cf9e9761b9
Manana1444 <manana11111444@gmail.com>
2020-12-30 16:23:38 +0100

    Made mod path dynamic

--- m_assi.py
+++ m_assi.py
@@ -1,15 +1,16 @@
-from os import listdir
+import os
 
-path = "D:\Documents\Paradox Interactive\Crusader Kings III\.mod assigner\mods"
-folders = listdir(path)
-y = "\ "
-x = y.strip()
+py_file= os.path.abspath(__file__) # path to main.py
+py_dir = os.path.dirname(py_file) # path to the parent dir of main.py
+
+mod_path = py_dir + "/mods"
+folders = os.listdir(mod_path)
 
 print(folders)
 for mod_name in folders:
     with open(mod_name + ".mod" , "a") as f:
-        path2 = "D:\Documents\Paradox Interactive\Crusader Kings III\.mod assigner\mods" + x +  str(mod_name)
-        with open(path2 + x +"descriptor.mod", "r") as f1:
+        path2 = mod_path + "/" +  str(mod_name)
+        with open(path2 + "/" +"descriptor.mod", "r") as f1:
             for text in f1:
                 f.write(text)
             f.write("\n" 'path = "mod/' + mod_name + '"')
